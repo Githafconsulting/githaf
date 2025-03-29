def62e159be941fc78c30e7a8ad2924170cd1b0a
Githafconsulting <gravitasitconsulting@gmail.com>
2025-03-29 16:21:03 +0400

    CompanyTicker.tsx

--- src/components/CompanyTicker.tsx
+++ src/components/CompanyTicker.tsx
@@ -1,41 +1,35 @@
-
 import React, { useEffect, useState } from 'react';
 
 const companies = [
   {
     id: 1,
     name: 'Al Ghurair',
-    logo: '/lovable-uploads/7c874dd2-2839-4ccc-b351-bd5cb000b4f8.png',
+    logo: '/lovable-uploads/agi.png', 
   },
   {
     id: 2,
     name: 'SABB',
-    logo: '/lovable-uploads/7110493e-eb9e-4bb9-89b9-b472c1fed90c.png',
+    logo: '/lovable-uploads/Sab.png',
   },
   {
     id: 3,
     name: 'PayPoint',
-    logo: '/lovable-uploads/2abf8e4b-445e-45db-a169-1b1bf48a80f1.png',
+    logo: '/lovable-uploads/paypoint.png',
   },
   {
     id: 4,
     name: 'RBS',
-    logo: '/lovable-uploads/f634365e-bac3-45cf-b24d-ffe0942da999.png',
+    logo: '/lovable-uploads/RBS.png',
   },
   {
     id: 5,
     name: 'Moody\'s Analytics',
-    logo: '/lovable-uploads/77ec07fe-550f-4e78-a595-153055987da2.png',
+    logo: '/lovable-uploads/Moodys.png',
   },
   {
     id: 6,
     name: 'HSBC',
-    logo: '/lovable-uploads/56b2d8ee-6c1f-40db-9af3-6dc95dd880f7.png',
-  },
-  {
-    id: 7,
-    name: 'LuupFX',
-    logo: '/lovable-uploads/ee62c6c3-4404-43d1-94c2-d74ecb019e88.png',
+    logo: '/lovable-uploads/hsbc.jpeg.jpg',
   },
 ];
 
@@ -75,7 +69,7 @@ const CompanyTicker: React.FC = () => {
           {!showFallback ? (
             <img 
               src={company.logo} 
-              alt={`${company.name} - Trusted client of Githaf Consulting`} 
+              
               className="h-12 w-auto max-w-full object-contain"
               loading="eager"
               onLoad={() => handleImageLoad(company.id, company.name)}
@@ -87,7 +81,6 @@ const CompanyTicker: React.FC = () => {
             </div>
           )}
         </div>
-        <p className="text-xs text-center mt-2 text-gray-600">{company.name}</p>
       </div>
     );
   };
